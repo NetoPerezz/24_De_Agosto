f7e61f0c80a767db524b9d8a214615364195bbc1
NetoPerezz <111472147+NetoPerezz@users.noreply.github.com>
2022-08-30 18:09:08 -0500

    Add files via upload

---
+++ restar.py
@@ -0,0 +1,4 @@
+'''Ernesto Perez 3D
+ 26 de Agosto 2022'''
+ def restar(a:int|float, b:int|float)->int|float:    # Se crea una funcion que pide un dato de tipo int o float y retorna un valor int o float
+	return a-b    # Retorna el valor de la resta del primer dato menos el segundo dato dado
