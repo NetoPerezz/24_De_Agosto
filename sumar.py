1bd41c64be31f289529bd3fd730ab072fd5a8cf3
NetoPerezz <111472147+NetoPerezz@users.noreply.github.com>
2022-08-30 18:11:05 -0500

    ops

---
+++ sumar.py
@@ -0,0 +1,4 @@
+'''Ernesto Perez 3D
+ 26 de Agosto 2022'''
+ def sumar(a:int|float, b:int|float) -> int|float:   # Se crea una funcion que pide un dato de tipo int o float y retorna un valor int o float
+	return a+b    # Retorna el valor de la suma de los dos datos dados
